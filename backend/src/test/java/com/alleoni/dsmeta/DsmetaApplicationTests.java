c5299e87ba4b1de8320af5201289413c0abb83b1
Guilherme Alleoni <guilherme.alleoni@yahoo.com.br>
2022-11-17 18:19:39 -0300

    Project created

---
+++ backend/src/test/java/com/alleoni/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alleoni.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
