ee34b07fa2f380210575f356719c76cc4c6ea465
Guilherme Alleoni <guilherme.alleoni@yahoo.com.br>
2022-11-18 18:40:45 -0300

    Database

---
+++ backend/src/main/java/com/alleoni/dsmeta/entities/Sale.java
@@ -0,0 +1,97 @@
+package com.alleoni.dsmeta.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity // Utilizado para fazer o mapeamento relacional com o banco de dados. Sua classe é equivalente há uma tabela no DB
+@Table(name = "tb_sales") // Cria-se uma tabela no DB.
+public class Sale implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id // Atributo Id será único (primary key)
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //Serve para que os Id's sejam numerados automaticamente
+	private Long id;
+	private String sellerName;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+	
+	public Sale() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSellerName() {
+		return sellerName;
+	}
+
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+
+	public Integer getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sale other = (Sale) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
