c9fb5fca615a70c9099ad78f47a09fc4eb8040fa
Guilherme Alleoni <guilherme.alleoni@yahoo.com.br>
2022-11-19 11:38:04 -0300

    Twilio SMS

--- backend/src/main/java/com/alleoni/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/alleoni/dsmeta/controllers/SaleController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alleoni.dsmeta.entities.Sale;
 import com.alleoni.dsmeta.services.SaleService;
+import com.alleoni.dsmeta.services.SmsService;
 
 @RestController // É o controller que vai disponibilizar os Endpoints para que o frontend acesse
 				// as informações.
@@ -20,6 +22,8 @@ public class SaleController {
 	@Autowired
 	private SaleService service; // Da mesma maneira que o Service, precisa do repository. O Controller, precisa
 									// do service. Controller > Service > Repository
+	@Autowired
+	private SmsService smsService;
 
 	/*
 	 * @GetMapping //Operações que tem para fazer uma requisição (Get serve para
@@ -32,9 +36,15 @@ public class SaleController {
 			@RequestParam(value = "maxDate", defaultValue = "") String maxDate,
 			Pageable pageable) {
 		return service.findSales(minDate, maxDate, pageable);
+		/*
+		 * Page e Pageable serve para buscar do banco de dados paginado, ou seja, com
+		 * menos linha por soliciação, por padrão, será retornado 20 linhas.
+		 */
 	}
-	/*
-	 * Page e Pageable serve para buscar do banco de dados paginado, ou seja, com
-	 * menos linha por soliciação, por padrão, será retornado 20 linhas.
-	 */
+	
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
+	}
+	
 }
