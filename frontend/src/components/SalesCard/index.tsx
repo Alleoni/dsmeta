b4042e6a6a760e7ec984bbd0c4822ba28c2671d4
Guilherme Alleoni <guilherme.alleoni@yahoo.com.br>
2022-11-19 15:27:55 -0300

    Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -22,6 +22,7 @@ function SalesCard() {
 
         const dmin = minDate.toISOString().slice(0, 10);
         const dmax = maxDate.toISOString().slice(0, 10);
+
         console.log(minDate);
         
 
@@ -29,9 +30,6 @@ function SalesCard() {
             setSales(response.data.content);
         })
     }, [minDate, maxDate]);
-
-
-
 
     return (
         <div className="dsmeta-card">
@@ -80,7 +78,7 @@ function SalesCard() {
                                     <td>R$ {sale.amount.toFixed(2)}</td>
                                     <td>
                                         <div className="dsmeta-red-btn-container">
-                                            <NotificationButton />
+                                            <NotificationButton saleId={sale.id} />
                                         </div>
                                     </td>
                                 </tr>
